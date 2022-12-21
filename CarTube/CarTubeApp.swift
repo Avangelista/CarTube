fcdb505d41479bc2ca8ec1ef2f723cc108fb4d2c
Rory Madden <rory.madden8@gmail.com>
2022-12-22 10:41:29 +1100

    Initial Commit

---
+++ CarTube/CarTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTubeApp.swift
+//  CarTube
+//
+//  Created by Rory Madden on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CarTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarTube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarTube
+//
+//  Created by Rory Madden on 22/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
